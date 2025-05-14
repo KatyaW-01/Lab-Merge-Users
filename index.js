c856928224fa23d88f4950e84c3972518ec6ba7f
Katya Weicht <katya.weicht@gmail.com>
2025-05-14 15:43:04 -0600

    Write first step of combined users function

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+function combineUsers(...usernames) {
+  let nestedArray = usernames
+  const combinedObject = {users: []}
 
+  for(i = 0; i < nestedArray.length; i ++){
+    combinedObject.users.push(...nestedArray[i])
+  }
+  
+  console.log(combinedObject)
+}
 
+combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"])
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
